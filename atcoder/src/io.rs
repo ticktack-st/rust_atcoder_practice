2041610c8d845506280fa5102946e96a98aa8447
ticktack-st <moglandwrapper@gmail.com>
2024-05-06 22:41:25 +0900

    add: stdin number

--- atcoder/src/io.rs
+++ atcoder/src/io.rs
@@ -5,3 +5,19 @@ pub fn read_textln() -> String {
     io::stdin().read_line(&mut buffer).expect("should String");
     buffer.trim().to_string()
 }
+
+pub fn read_u32() -> u32 {
+    let mut buffer = String::new();
+    match io::stdin().read_line(&mut buffer) {
+        Ok(_) => buffer.trim().parse::<u32>().expect("should number"),
+        Err(e) => panic!("error: {}", e),
+    }
+}
+
+pub fn read_i32() -> i32 {
+    let mut buffer = String::new();
+    match io::stdin().read_line(&mut buffer) {
+        Ok(_) => buffer.trim().parse::<i32>().expect("should number"),
+        Err(e) => panic!("error: {}", e),
+    }
+}
